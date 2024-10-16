21c522210a34fea99be9d08e1fa80d7d54c7af0d
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-10-16 09:48:02 +0900

    ユーザー登録とログイン機能の作成

--- db/migrate/20241015065208_sorcery_core.rb
+++ db/migrate/20241015065208_sorcery_core.rb
@@ -4,8 +4,8 @@ class SorceryCore < ActiveRecord::Migration[7.2]
     # その後に続くのがカラム名
     # 例⇒t.string :emailは、string型のemailカラムを意味する
     create_table :users do |t|
-    # ●null: falseについて
-    # データベースの該当カラムにNULL制約（NULL値の保存を許容しない）を設けるための記述
+      # ●null: falseについて
+      # データベースの該当カラムにNULL制約（NULL値の保存を許容しない）を設けるための記述
       t.string :email,            null: false, index: { unique: true }
       # ●index: { unique: true }について
       # データベースの該当カラムにインデックスを設定し、ユニーク制約（重複する値の保存を許容しない）を加える記述
@@ -19,9 +19,3 @@ class SorceryCore < ActiveRecord::Migration[7.2]
     end
   end
 end
-
-
-
-
-
-

f976507e91e09890ffaefeee9d7806a8cd7f7edb
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-10-22 16:00:27 +0900

    掲示板修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,19 +10,19 @@ class StaticPagesController < ApplicationController
     # 各料理掲示板一覧アクションを追加して、レンダリングするようにする
     def details
         @recipe = params[:recipe]
-    
+
         # 料理に応じた情報をハッシュで定義
         recipes = {
-          "ojakhuri" => { name: "オジャフリ", image: "Answer01.png", description: I18n.t('diagnoses.description.ojakhuri2') },
-          "badrijani" => { name: "パドリジャーニ", image: "Answer02.png", description: I18n.t('diagnoses.description.badrijani2')},
-          "sokos" => { name: "ソコスチャシュシュリ", image: "Answer03.png", description: I18n.t('diagnoses.description.sokos_chashushuli2')},
-          "pkhali" => { name: "プパリ", image: "Answer04.png", description: I18n.t('diagnoses.description.pkhali5')},
-          "ostri" => { name: "オーストリ", image: "Answer05.png", description: I18n.t('diagnoses.description.ostri2')},
-          "chikirtma" => { name: "チヒルトゥマ", image: "Answer06.png", description: I18n.t('diagnoses.description.chikirtma3')},
-          "shkmeruli" => { name: "シュクメルリ", image: "Answer07.png", description: I18n.t('diagnoses.description.shkmeruli4')},
-          "chakhohbili" => { name: "チャホフビリ", image: "Answer08.png", description: I18n.t('diagnoses.description.chakhohbili2')},
+          "ojakhuri" => { name: "オジャフリ", image: "Answer01.png", description: I18n.t("diagnoses.description.ojakhuri2") },
+          "badrijani" => { name: "パドリジャーニ", image: "Answer02.png", description: I18n.t("diagnoses.description.badrijani2") },
+          "sokos" => { name: "ソコスチャシュシュリ", image: "Answer03.png", description: I18n.t("diagnoses.description.sokos_chashushuli2") },
+          "pkhali" => { name: "プパリ", image: "Answer04.png", description: I18n.t("diagnoses.description.pkhali5") },
+          "ostri" => { name: "オーストリ", image: "Answer05.png", description: I18n.t("diagnoses.description.ostri2") },
+          "chikirtma" => { name: "チヒルトゥマ", image: "Answer06.png", description: I18n.t("diagnoses.description.chikirtma3") },
+          "shkmeruli" => { name: "シュクメルリ", image: "Answer07.png", description: I18n.t("diagnoses.description.shkmeruli4") },
+          "chakhohbili" => { name: "チャホフビリ", image: "Answer08.png", description: I18n.t("diagnoses.description.chakhohbili2") }
         }
-    
+
         @recipe_info = recipes[@recipe]
     end
 end

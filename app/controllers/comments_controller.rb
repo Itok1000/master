43ae01c1a7f109907ca45242fd0a49e4d54ceb44
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-10-24 18:07:51 +0900

    掲示板詳細

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        redirect_to post_path(@post), success: t('defaults.flash_message.created', item: Comment.model_name.human)
+      else
+        @comments = @post.comments.includes(:user).order(created_at: :desc)
+        flash.now[:danger] = t('defaults.flash_message.not_created', item: Comment.model_name.human)
+        render 'posts/show', status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+    end
+  end

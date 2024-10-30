cfe3eaa6b2d5d75c2a0bbe153c0dbc024a9c9d0d
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-10-30 10:59:35 +0900

    collection訂正、いいねコントローラーの見直しによるいいね一覧の表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,25 @@
 class FavoritesController < ApplicationController
     before_action :find_post
-
+    # いいねをするメソッド
     def create
         post = Post.find(params[:post_id])
-        favorite = post.favorites.build(user_id: current_user.id)
+    #　投稿の新規いいねを現在ログインしているユーザーにセットする
+        favorite = @post.favorites.new(user: current_user)
+       #いいねすると、「いいねしました」とフラッシュメッセージを放つ
         favorite.save
-        redirect_to request.referer, success: t(".success") # リファラで前ページに戻る
+            redirect_to request.referer, success: t(".success") # リファラで前ページに戻る
     end
-
+    # いいねを取り消しするメソッド
     def destroy
         post = Post.find(params[:post_id])
-        favorite = current_user.favorites.find(params[:id])
-        favorite.destroy!
-        redirect_to request.referer, success: t(".deleted") # リファラで前ページに戻る
+    #　投稿のいいねを現在ログインしているユーザーに基づいて探す
+        favorite = @post.favorites.find_by(user: current_user)
+    #いいねを取り消しすると、「いいねを取り消しました」とフラッシュメッセージを放つ
+            favorite.destroy
+            redirect_to request.referer, success: t(".deleted") # リファラで前ページに戻る
     end
 
     private
-
     def find_post
         @post = Post.find(params[:post_id])
     end

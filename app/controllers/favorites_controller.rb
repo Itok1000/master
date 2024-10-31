823f6d68af440cbc468ceab297dd982b4710f6cc
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-10-31 10:48:02 +0900

    いいね機能微修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,7 +7,7 @@ class FavoritesController < ApplicationController
         favorite = @post.favorites.new(user: current_user)
         # いいねすると、「いいねしました」とフラッシュメッセージを放つ
         favorite.save
-            redirect_to request.referer, success: t(".success") # リファラで前ページに戻る
+            redirect_to request.referer # リファラで前ページに戻る
     end
     # いいねを取り消しするメソッド
     def destroy
@@ -16,7 +16,7 @@ class FavoritesController < ApplicationController
         favorite = @post.favorites.find_by(user: current_user)
             # いいねを取り消しすると、「いいねを取り消しました」とフラッシュメッセージを放つ
             favorite.destroy
-            redirect_to request.referer, success: t(".deleted") # リファラで前ページに戻る
+            redirect_to request.referer # リファラで前ページに戻る
     end
 
     private

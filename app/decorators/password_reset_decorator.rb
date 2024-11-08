51951db81a00405a09d49b0fddd64722e89e9da1
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-11-08 15:46:59 +0900

    パスワードリセット

--- app/decorators/password_reset_decorator.rb
+++
@@ -1,5 +0,0 @@
-class PasswordResetDecorator < Draper::Decorator
-    delegate_all
-  # delegate_allは、デコレータに追加されていない機能が呼び出されたときに、その機能を元のオブジェクトに任せるための指定
-  # 例えば、友達に電話で質問があるとき、その質問に答えられない場合は、他の友達に質問をまわすようなイメージ
-end

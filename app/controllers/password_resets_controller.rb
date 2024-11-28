bb5111358ca47c5197dbfc2fea14562275939511
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-11-28 13:49:26 +0900

    パスワードリセット

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,8 +2,8 @@ class PasswordResetsController < ApplicationController
   skip_before_action :require_login
   def new; end
 
-  # request password reset.
-  # you get here when the user entered their email in the reset password form and submitted it.
+  # パスワードリセットをリクエストする。
+  # ユーザがパスワードリセットフォームに電子メールを入力し、送信した場合に表示される
   def create
     @user = User.find_by(email: params[:email])
 
@@ -16,7 +16,7 @@ class PasswordResetsController < ApplicationController
     redirect_to login_path, success: t(".success")
   end
 
-  # This is the reset password form.
+  # パスワードリセット(編集)するためのフォーム
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
@@ -27,7 +27,7 @@ class PasswordResetsController < ApplicationController
     nil
   end
 
-  # This action fires when the user has sent the reset password form.
+  # ユーザがパスワードリセットフォームを送信したときに発生
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
@@ -37,9 +37,9 @@ class PasswordResetsController < ApplicationController
       return
     end
 
-    # the next line makes the password confirmation validation work
+    # パスワード確認のバリデーションを機能させる
     @user.password_confirmation = params[:user][:password_confirmation]
-    # the next line clears the temporary token and updates the password
+    # 一時トークンをクリアし、パスワードを更新する
     if @user.change_password(params[:user][:password])
       redirect_to(login_path, success: t(".success"))
     else

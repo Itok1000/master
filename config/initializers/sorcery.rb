5efd924d4392a9ac66468901b69781f1dab560bf
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-04 18:45:52 +0900

    Google認証やり直し

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -424,124 +424,124 @@ Rails.application.config.sorcery.configure do |config|
      #
      user.reset_password_time_between_emails = 1 * 1  # 必要に応じて再送信間隔を設定
 
-     # Access counter to a reset password page attribute name
-     # Default: `:access_count_to_reset_password_page`
-     #
-     # user.reset_password_page_access_count_attribute_name =
+    # Access counter to a reset password page attribute name
+    # Default: `:access_count_to_reset_password_page`
+    #
+    # user.reset_password_page_access_count_attribute_name =
 
-     # -- magic_login --
-     # Magic login code attribute name.
-     # Default: `:magic_login_token`
-     #
-     # user.magic_login_token_attribute_name =
+    # -- magic_login --
+    # Magic login code attribute name.
+    # Default: `:magic_login_token`
+    #
+    # user.magic_login_token_attribute_name =
 
-     # Magic login expiry attribute name.
-     # Default: `:magic_login_token_expires_at`
-     #
-     # user.magic_login_token_expires_at_attribute_name =
+    # Magic login expiry attribute name.
+    # Default: `:magic_login_token_expires_at`
+    #
+    # user.magic_login_token_expires_at_attribute_name =
 
-     # When was magic login email sent — used for hammering protection.
-     # Default: `:magic_login_email_sent_at`
-     #
-     # user.magic_login_email_sent_at_attribute_name =
+    # When was magic login email sent — used for hammering protection.
+    # Default: `:magic_login_email_sent_at`
+    #
+    # user.magic_login_email_sent_at_attribute_name =
 
-     # REQUIRED:
-     # Magic login mailer class.
-     # Default: `nil`
-     #
-     # user.magic_login_mailer_class =
+    # REQUIRED:
+    # Magic login mailer class.
+    # Default: `nil`
+    #
+    # user.magic_login_mailer_class =
 
-     # Magic login email method on your mailer class.
-     # Default: `:magic_login_email`
-     #
-     # user.magic_login_email_method_name =
+    # Magic login email method on your mailer class.
+    # Default: `:magic_login_email`
+    #
+    # user.magic_login_email_method_name =
 
-     # When true, sorcery will not automatically
-     # send magic login details email, and allow you to
-     # manually handle how and when the email is sent
-     # Default: `true`
-     #
-     # user.magic_login_mailer_disabled =
+    # When true, sorcery will not automatically
+    # send magic login details email, and allow you to
+    # manually handle how and when the email is sent
+    # Default: `true`
+    #
+    # user.magic_login_mailer_disabled =
 
-     # How many seconds before the request expires. nil for never expires.
-     # Default: `nil`
-     #
-     # user.magic_login_expiration_period =
+    # How many seconds before the request expires. nil for never expires.
+    # Default: `nil`
+    #
+    # user.magic_login_expiration_period =
 
-     # Hammering protection: how long in seconds to wait before allowing another email to be sent.
-     # Default: `5 * 60`
-     #
-     # user.magic_login_time_between_emails =
+    # Hammering protection: how long in seconds to wait before allowing another email to be sent.
+    # Default: `5 * 60`
+    #
+    # user.magic_login_time_between_emails =
 
-     # -- brute_force_protection --
-     # Failed logins attribute name.
-     # Default: `:failed_logins_count`
-     #
-     # user.failed_logins_count_attribute_name =
+    # -- brute_force_protection --
+    # Failed logins attribute name.
+    # Default: `:failed_logins_count`
+    #
+    # user.failed_logins_count_attribute_name =
 
-     # This field indicates whether user is banned and when it will be active again.
-     # Default: `:lock_expires_at`
-     #
-     # user.lock_expires_at_attribute_name =
+    # This field indicates whether user is banned and when it will be active again.
+    # Default: `:lock_expires_at`
+    #
+    # user.lock_expires_at_attribute_name =
 
-     # How many failed logins are allowed.
-     # Default: `50`
-     #
-     # user.consecutive_login_retries_amount_limit =
+    # How many failed logins are allowed.
+    # Default: `50`
+    #
+    # user.consecutive_login_retries_amount_limit =
 
-     # How long the user should be banned, in seconds. 0 for permanent.
-     # Default: `60 * 60`
-     #
-     # user.login_lock_time_period =
+    # How long the user should be banned, in seconds. 0 for permanent.
+    # Default: `60 * 60`
+    #
+    # user.login_lock_time_period =
 
-     # Unlock token attribute name
-     # Default: `:unlock_token`
-     #
-     # user.unlock_token_attribute_name =
+    # Unlock token attribute name
+    # Default: `:unlock_token`
+    #
+    # user.unlock_token_attribute_name =
 
-     # Unlock token mailer method
-     # Default: `:send_unlock_token_email`
-     #
-     # user.unlock_token_email_method_name =
+    # Unlock token mailer method
+    # Default: `:send_unlock_token_email`
+    #
+    # user.unlock_token_email_method_name =
 
-     # When true, sorcery will not automatically
-     # send email with the unlock token
-     # Default: `false`
-     #
-     # user.unlock_token_mailer_disabled = true
+    # When true, sorcery will not automatically
+    # send email with the unlock token
+    # Default: `false`
+    #
+    # user.unlock_token_mailer_disabled = true
 
-     # REQUIRED:
-     # Unlock token mailer class.
-     # Default: `nil`
-     #
-     # user.unlock_token_mailer =
+    # REQUIRED:
+    # Unlock token mailer class.
+    # Default: `nil`
+    #
+    # user.unlock_token_mailer =
 
-     # -- activity logging --
-     # Last login attribute name.
-     # Default: `:last_login_at`
-     #
-     # user.last_login_at_attribute_name =
+    # -- activity logging --
+    # Last login attribute name.
+    # Default: `:last_login_at`
+    #
+    # user.last_login_at_attribute_name =
 
-     # Last logout attribute name.
-     # Default: `:last_logout_at`
-     #
-     # user.last_logout_at_attribute_name =
+    # Last logout attribute name.
+    # Default: `:last_logout_at`
+    #
+    # user.last_logout_at_attribute_name =
 
-     # Last activity attribute name.
-     # Default: `:last_activity_at`
-     #
-     # user.last_activity_at_attribute_name =
+    # Last activity attribute name.
+    # Default: `:last_activity_at`
+    #
+    # user.last_activity_at_attribute_name =
 
-     # How long since user's last activity will they be considered logged out?
-     # Default: `10 * 60`
-     #
-     # user.activity_timeout =
+    # How long since user's last activity will they be considered logged out?
+    # Default: `10 * 60`
+    #
+    # user.activity_timeout =
 
-     # -- external --
-     # Class which holds the various external provider data for this user.
-     # Default: `nil`
-     #
-     # user.authentications_class = Authentication
+    # -- external --
+    # Class which holds the various external provider data for this user.
+    # Default: `nil`
+    #
+    # user.authentications_class = Authentication
 
     # User's identifier in the `authentications` class.
     # Default: `:user_id`

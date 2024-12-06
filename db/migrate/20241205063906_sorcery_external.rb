b1483794954aa2d09fbec8f34281d63e7a853cc6
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-06 16:40:58 +0900

    Google認証(本番環境成功)

--- db/migrate/20241205063906_sorcery_external.rb
+++ db/migrate/20241205063906_sorcery_external.rb
@@ -1,10 +1,14 @@
 class SorceryExternal < ActiveRecord::Migration[7.2]
   def change
-    create_table :authentications do |t|
-      t.integer :user_id, null: false
-      t.string :provider, :uid, null: false
-      t.timestamps              null: false
+    # まずテーブルが存在しないか確認して、存在しなければ新しくテーブルを作成してる。
+    unless table_exists?(:authentications)
+      create_table :authentications do |t|
+        t.integer :user_id, null: false
+        t.string :provider, :uid, null: false
+        t.timestamps null: false
+      end
     end
-    add_index :authentications, [ :provider, :uid ]
+    # さらに、インデックスの追加もチェックしている
+    add_index :authentications, [:provider, :uid] unless index_exists?(:authentications, [:provider, :uid])
   end
 end

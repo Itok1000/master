89c0986b59b55ccd50be8d7f0a6edb5f1e3808ab
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-11 15:32:03 +0900

    ユーザーのモデルテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "バリデーションチェック" do
+    it "バリデーションが機能しているか" do
+      user = build(:user)
+      expect(user).to be_valid
+      expect(user.errors).to be_empty
+    end
+
+    it "名前がない場合、エラーになる" do
+      no_name = build(:user, user_name: "")
+      expect(no_name).to be_invalid
+    end
+
+    it "名前が被る場合、エラーになる" do
+      create(:user, user_name: "らんてくん")
+      other = build(:user, user_name: "らんてくん")
+      expect(other).to be_invalid
+    end
+
+    it "名前の文字数が長すぎる場合、エラーになる" do
+      user = build(:user, user_name: "a" * 256)
+      expect(user).to be_invalid
+    end
+  end
+end

c8ef095616457bd599064b502bc814a1940db1af
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-11 15:58:04 +0900

    ユーザーのモデルテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,20 +8,40 @@ RSpec.describe User, type: :model do
       expect(user.errors).to be_empty
     end
 
-    it "名前がない場合、エラーになる" do
+    it "ユーザーネームが入力されていること" do
       no_name = build(:user, user_name: "")
       expect(no_name).to be_invalid
     end
 
-    it "名前が被る場合、エラーになる" do
+    it "名前が被らないこと" do
       create(:user, user_name: "シュクメルリ君")
       other = build(:user, user_name: "シュクメルリ君")
       expect(other).to be_invalid
     end
 
-    it "名前の文字数が長すぎる場合、エラーになる" do
+    it "名前の文字数が長すぎないこと" do
       user = build(:user, user_name: "a" * 256)
       expect(user).to be_invalid
+    end
+
+    it "パスワードが3文字以下でないこと" do
+      user = build(:user, password: "a" * 3)
+      expect(user).to be_invalid
+    end
+
+    it "パスワード入力されていること" do
+      no_password = build(:user, password: "")
+      expect(no_password).to be_invalid
+    end
+
+    it "確認用パスワードが入力されていること" do
+      no_password_confirmation = build(:user, password_confirmation: "")
+      expect(no_password_confirmation).to be_invalid
+    end
+
+    it "パスワードと確認用パスが一致すること" do
+      user = build(:user, password: "gamarjoba", password_confirmation: "gamarjoba")
+      expect(user).to be_valid
     end
   end
 end

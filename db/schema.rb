79b796e7d41028e71d4b796088f3a5254048705d
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-17 14:44:36 +0900

    レーティング機能やり直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_13_021121) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_05_063906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,17 +50,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_13_021121) do
     t.index ["user_id", "post_id"], name: "index_favorites_on_user_id_and_post_id", unique: true
   end
 
-  create_table "feedbacks", force: :cascade do |t|
-    t.float "rating", default: 0.0, null: false
-    t.text "content"
-    t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_feedbacks_on_post_id"
-    t.index ["user_id"], name: "index_feedbacks_on_user_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -89,7 +78,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_13_021121) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "feedbacks", "posts"
-  add_foreign_key "feedbacks", "users"
   add_foreign_key "posts", "users"
 end

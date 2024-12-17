a8299d577e644bda94b1ca59fb10194e9bb582af
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-17 17:02:10 +0900

    ratyを用いたレーティング機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,15 @@
 // Entry point for the build script in your package.json
+import Raty from './raty'; // 相対パスでraty.jsをインポート
+window.raty = function(elem, opt) {
+  let raty = new Raty(elem, opt);
+  raty.init();
+  return raty;
+}
+
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"

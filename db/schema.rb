259b6f6690f2ab32de9d627baee27221706bb349
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-19 11:18:56 +0900

    ratyを用いたレーティング機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_05_063906) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_19_002158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_05_063906) do
     t.datetime "updated_at", null: false
     t.string "recipe"
     t.string "post_image"
+    t.float "star"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

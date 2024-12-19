ce3f6c5692c6aef3522a784504b05ed0df23d8d7
伊藤健太 <jiantaiyiteng1@gmail.com>
2024-12-19 12:05:52 +0900

    口コミ機能のスペックテスト

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -7,13 +7,21 @@ RSpec.describe "Posts", type: :system do
   describe "ログイン前" do
     it "口コミ一覧が見れないこと" do
       visit "posts?recipe=ojakhuri"
+      Capybara.assert_current_path("/login", ignore_query: true)
+      expect(current_path).to eq('/login')
       expect(page).to have_content 'ログインしてください'
     end
   end
 
   describe "ログイン後" do
-    # context '口コミがある場合' do
+    context '口コミがある場合' do
+      it "料理のリンクから口コミ一覧へ遷移できること" do
+        
+      end
+    end
 
-    # end
+    context '口コミがない場合' do
+
+    end
   end
 end

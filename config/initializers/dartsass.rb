517de1536ea4fcddf75da9c71a09da1c4bf90105
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-14 10:58:08 +0900

    指定の JavaScript パッケージをインストール

--- config/initializers/dartsass.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.dartsass.builds = {
-  "application.scss" => "application.css",
-  "admin/application.scss" => "admin/application.css",
-  "admin/login.scss" => "admin/login.css"
-}

cd577a54b8b98dc0d8ca7ffcfdfc33ccfe079cf1
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-03-05 14:36:22 +0900

    本番環境におけるログイン管理画面のエラー解決

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -1,16 +1,2 @@
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap/dist/js/bootstrap"
-(() => {
-    const sidebarToggleButton = document.querySelector("#toggleSidebar");
-    const mainArea = document.querySelector('main')
-    sidebarToggleButton.addEventListener('click', () => {
-      const sidebar = document.querySelector("#sidebarMenu");
-      if(sidebar.classList.contains("sidebar-closed")){
-        sidebar.classList.remove("sidebar-closed");
-        mainArea.classList.remove("w-100");
-      } else {
-        sidebar.classList.add("sidebar-closed");
-        mainArea.classList.add("w-100");
-      }
-    })
-  })()

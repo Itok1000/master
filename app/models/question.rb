c2c9fe71571873f6f841542efce9d69b3c14722a
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-04-16 18:20:48 +0900

    クイズ機能におけるi18nの導入

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,17 +5,16 @@ class Question < ActiveYaml::Base
     def localized_content
         content[I18n.locale.to_s]
     end
-      
+
     def localized_choices
         choices[I18n.locale.to_s]
     end
-      
+
     def localized_answers
         answers[I18n.locale.to_s]
     end
-      
+
     def localized_explain
         explain[I18n.locale.to_s]
     end
-      
 end

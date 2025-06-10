e9b0c7ffefdbe5747734b336f8724d5877e30d9a
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-06-10 16:55:17 +0900

    ヘッダーとフッターのシステムスペックテストの実装

---
+++ spec/system/commons_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe '共通系', type: :system do
+  context 'ログイン前' do
+    before do
+      visit root_path
+    end
+    describe 'ヘッダー' do
+      it 'ヘッダーが正しく表示されていること' do
+        expect(page).to have_content('ログイン'), 'ヘッダーに「ログイン」というテキストが表示されていること'
+        expect(page).to have_content('新規登録'), 'ヘッダーに「新規登録」というテキストが表示されていること'
+      end
+    end
+
+    describe 'フッター' do
+      it 'フッターが正しく表示されていること' do
+        expect(page).to have_content('© 2024. Itok1000'), '「© 2024. Itok1000」というテキストが表示されていること'
+        expect(page).to have_content('利用規約'), '「利用規約」というテキストが表示されていること'
+        expect(page).to have_content('プライバシーポリシー'), '「プライバシーポリシー」というテキストが表示されていること'
+        expect(page).to have_content('お問い合わせ'), '「お問い合わせ」というテキストが表示されていること'
+      end
+    end
+  end
+end

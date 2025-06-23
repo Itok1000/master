0525c1edd5c8e5a500160d71b71edcc5aa637876
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-06-23 15:44:04 +0900

    クイズ機能にタイトル動的の追加した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,41 @@
 module ApplicationHelper
-    def page_title(title = "", admin: false)
-        base_title = admin ? "ガマルジョバ/გამარჯობა(管理画面)" : "ガマルジョバ/გამარჯობა"
-        title.present? ? "#{title} | #{base_title}" : base_title
-    end
+  def page_title(title = '', admin: false)
+    base_title = admin ? 'ガマルジョバ/გამარჯობა(管理画面)' : 'ガマルジョバ/გამარჯობა'
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
 
-    def active_if(path)
-        path == controller_path ? "active" : ""
-    end
+  def active_if(path)
+    path == controller_path ? "active" : ""
+  end
 
-    def default_meta_tags
-        {
-          site: "ガマルジョバ",
-          title: "ガマルジョバ/გამარჯობა",
-          reverse: true,
-          charset: "utf-8",
-          description: "ジョージア料理診断サービスWebアプリ ログインなしで簡単な質問をすれば、おすすめのジョージア料理をご紹介！ (Xでシェアもできるよ) ログインして、料理の口コミやいいねをしてジョージア料理の奥深さを感じてみよう！",
-          keywords: "ジョージア,ジョージア料理,ジョージア料理診断アプリ,georgia,gamarjoba,გამარჯობა,საქართველო",
-          canonical: request.original_url,
-          separator: "|",
-          icon: [
-            { href: image_url("logo.png") },
-            { href: image_url("logo.png"), rel: "apple-touch-icon", sizes: "600x315", type: "image/png" }
-          ],
-          og: {
-            site_name: :site,
-            title: :title,
-            description: :description,
-            type: "website",
-            url: request.original_url,
-            image: image_url("ジョージア料理診断アプリ ガマルジョバ.png"),
-            local: "ja-JP"
-          },
-          twitter: {
-            card: "summary_large_image",
-            site: "@Itoken1000",
-            image: image_url("ジョージア料理診断アプリ ガマルジョバ.png")
-          }
+  def default_meta_tags
+    {
+      site: "ガマルジョバ",
+      title: "ガマルジョバ/გამარჯობა",
+      reverse: true,
+      charset: "utf-8",
+      description: "ジョージア料理診断サービスWebアプリ ログインなしで簡単な質問をすれば、おすすめのジョージア料理をご紹介！ (Xでシェアもできるよ) ログインして、料理の口コミやいいねをしてジョージア料理の奥深さを感じてみよう！",
+      keywords: "ジョージア,ジョージア料理,ジョージア料理診断アプリ,georgia,gamarjoba,გამარჯობა,საქართველო",
+      canonical: request.original_url,
+      separator: "|",
+        icon: [
+          { href: image_url("logo.png") },
+          { href: image_url("logo.png"), rel: "apple-touch-icon", sizes: "600x315", type: "image/png" }
+        ],
+        og: {
+          site_name: :site,
+          title: :title,
+          description: :description,
+          type: "website",
+          url: request.original_url,
+          image: image_url("ジョージア料理診断アプリ ガマルジョバ.png"),
+          local: "ja-JP"
+        },
+        twitter: {
+          card: "summary_large_image",
+          site: "@Itoken1000",
+          image: image_url("ジョージア料理診断アプリ ガマルジョバ.png")
         }
-    end
+    }
+  end
 end

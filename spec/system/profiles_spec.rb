75309212f04347f964757fc2c66d7e9560eec90c
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-07-09 11:38:15 +0900

    プロフィール設定のシステムスペックテスト追加

--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -19,5 +19,25 @@ RSpec.describe "Profiles", type: :system do
         expect(page).to have_content(user.decorate.full_name), 'プロフィールページにユーザー名が表示されていること'
         expect(page).to have_content(user.email), 'プロフィールページにメールアドレスが表示されていること'
     end
+
+    it "プロフィールの編集ができること(メールアドレス)" do
+      find('#navbarDropdown').click
+      click_on 'プロフィール設定'
+      click_on '編集'
+      expect(page).to have_content('プロフィール編集'), 'プロフィール編集ページに「プロフィール編集」というテキストが表示されていること'
+      fill_in "メールアドレス", with: "new_email@test.com"
+      click_button "更新"
+      expect(page).to have_content('ユーザーを更新しました'), 'プロフィール更新後に成功メッセージが表示されること'
+    end
+
+    it "プロフィールの編集ができること(ユーザーネーム)" do
+      find('#navbarDropdown').click
+      click_on 'プロフィール設定'
+      click_on '編集'
+      expect(page).to have_content('プロフィール編集'), 'プロフィール編集ページに「プロフィール編集」というテキストが表示されていること'
+      fill_in "ユーザーネーム", with: "新しいユーザー名"
+      click_button "更新"
+      expect(page).to have_content('ユーザーを更新しました'), 'プロフィール更新後に成功メッセージが表示されること'
+    end
   end
 end
